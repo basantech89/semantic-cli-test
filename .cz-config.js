106bf9299ef5f3fc3a14398ed9b9f4d993b20758
BASANT SONI <basant@LAPTOP-BA8VSS9D.localdomain>
2022-11-09 21:27:15 +0530

    :sparkles: feat(core): add core tool

---
+++ .cz-config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  types: [
+    { value: ":sparkles: feat", name: "✨ feat:\tAdding a new feature" },
+    { value: ":bug: fix", name: "🐛 fix:\tFixing a bug" },
+    { value: ":ambulance: hotfix", name: "🚑 hotfix:\tCritical hotfix" },
+    { value: ":memo: docs", name: "📝 docs:\tAdd or update documentation" },
+    {
+      value: ":lipstick: style",
+      name: "💄 style:\tAdd or update styles, ui or ux",
+    },
+    {
+      value: ":recycle: refactor",
+      name: "♻️ refactor:\tCode change that neither fixes a bug nor adds a feature",
+    },
+    {
+      value: ":zap: perf",
+      name: "⚡️ perf:\tCode change that improves performance",
+    },
+    {
+      value: ":white_check_mark: test",
+      name: "✅ test:\tAdding tests cases",
+    },
+    {
+      value: ":truck: chore",
+      name: "🚚 chore:\tChanges to the build process or auxiliary tools\n\t\tand libraries such as documentation generation",
+    },
+    { value: ":rewind: revert", name: "⏪️ revert:\tRevert to a commit" },
+    { value: ":construction: wip", name: "🚧 wip:\tWork in progress" },
+    {
+      value: ":construction_worker: build",
+      name: "👷 build:\tAdd or update regards to build process",
+    },
+    {
+      value: ":green_heart: ci",
+      name: "💚 ci:\tFixing CI build",
+    },
+    {
+      value: ":lock: security",
+      name: "🔒 security:\tFixing security issues",
+    },
+    {
+      value: ":tada: init",
+      name: "🎉 init:\tInitial commit",
+    },
+  ],
+
+  scopes: [
+    { name: "ui" },
+    { name: "ci" },
+    { name: "tests" },
+    { name: "core" },
+    { name: "tools" },
+  ],
+
+  scopeOverrides: {
+    fix: [
+      { name: "merge" },
+      { name: "style" },
+      { name: "test" },
+      { name: "hotfix" },
+    ],
+  },
+
+  allowCustomScopes: true,
+  allowBreakingChanges: ["feat", "fix"],
+  // skip any questions you want
+  skipQuestions: ["body"],
+  subjectLimit: 100,
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const selectLine = require("inquirer-select-line");
+
+inquirer.registerPrompt("selectLine", selectLine);
+inquirer
+  .prompt([
+    {
+      type: "selectLine",
+      message: "Where add line?",
+      name: "line",
+      choices: ["first", "second", "third", "fourth"],
+    },
+  ])
+  .then(function (answers) {
+    console.log("Chosen line: ", answers?.line);
+    /*
+    OUTPUT :
+    Chosen line: 2
+    */
+  });
