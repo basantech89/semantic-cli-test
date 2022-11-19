49ca133d7d91b1462231b383f4430db11fb25f70
BASANT SONI <basant@EMPID20018.localdomain>
2022-11-19 12:14:54 +0530

    feat(core): :sparkles: rferf

--- .cz-config.js
+++ .cz-config.js
@@ -50,15 +50,24 @@ module.exports = {
     { name: "tests" },
     { name: "core" },
     { name: "tools" },
+    { name: "style" },
   ],
 
   scopeOverrides: {
-    fix: [
-      { name: "merge" },
-      { name: "style" },
-      { name: "test" },
-      { name: "hotfix" },
-    ],
+    fix: [{ name: "style" }, { name: "tests" }],
+  },
+
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: "\nDenote the SCOPE of this change (optional):",
+    // used if allowCustomScopes is true
+    customScope: "Denote the SCOPE of this change:",
+    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: "List any BREAKING CHANGES:\n",
+    footer:
+      "List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n",
+    confirmCommit: "Are you sure you want to proceed with the commit above?",
   },
 
   allowCustomScopes: true,
