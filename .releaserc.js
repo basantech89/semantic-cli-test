42e71b9cdf95f071f35da2920bc1c268f868944c
BASANT SONI <basant@EMPID20018.localdomain>
2022-11-26 17:30:21 +0530

    :sparkles: module(core): edewdewd

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,5 @@
 const commitTypes = require("./commitTypes");
+
 const parserOpts = {
   headerPattern:
     /^(?::\w*:|(?:\ud83c[\udf00-\udfff])|(?:\ud83d[\udc00-\ude4f\ude80-\udeff])|[\u2600-\u2B55])\s(?<type>\w*)(?:\((?<scope>.*)\))?!?:\s(?<subject>(?:(?!#).)*(?:(?!\s).))(?:\s\(?\)?)?$/,
