1f8ed55a11ad5707906a4541312037f0b7671994
BASANT SONI <basant@EMPID20018.localdomain>
2022-11-28 09:53:55 +0530

    :sparkles: module(core): ewdewdw

--- .releaserc.js
+++ .releaserc.js
@@ -68,6 +68,7 @@ module.exports = {
     [
       "@semantic-release/git",
       {
+        assets: ["package.json", "npm-shrinkwrap.json"],
         message:
           "🔖 chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
